796baed2b496cf6080423e4af18405f99944c8a6
Jan Boerman <Janboerman95@gmail.com>
2019-12-05 16:18:48 +0100

    initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "AdventOfCode2019"
+
+version := "1.0"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.13" % "3.1.0" % "test"
---
+++ src/main/scala/com/janboerman/aoc2019/day1/Day1.scala
@@ -0,0 +1,33 @@
+import scala.io.Source
+
+object Day1 extends App {
+
+    val fileName = "src/main/resources/day1input.txt"
+    val result1 = Source.fromFile(fileName).getLines().map(_.toDouble).map(requiredFuel).sum
+    val result2 = Source.fromFile(fileName).getLines().map(_.toDouble).map(recursiveFuel).sum
+
+//    println(requiredFuel(12D))
+//    println(requiredFuel(14D))
+//    println(requiredFuel(1969D))
+//    println(requiredFuel(100756D))
+
+//    println(recursiveFuel(14D))
+//    println(recursiveFuel(1969D))
+//    println(recursiveFuel(100756D));
+
+    println(result1)
+    println(result2)
+
+    def requiredFuel(componentMass: Double): Double = {
+        val dividedByThree = componentMass / 3
+        val floored = Math.floor(dividedByThree)
+        val subtracted = floored - 2D
+        subtracted
+    }
+
+    def recursiveFuel(componentMass: Double): Double = {
+        val fuel = requiredFuel(componentMass)
+        if (fuel < 0) 0D else fuel + recursiveFuel(fuel)
+    }
+
+}
