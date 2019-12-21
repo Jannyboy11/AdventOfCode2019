d264ab5cf12003ea38888bc1a0ffd58fcd2ca855
Jan Boerman <Janboerman95@gmail.com>
2019-12-21 11:02:04 +0100

    16a

--- src/main/scala/com/janboerman/aoc2019/day16/Day16.scala
+++ src/main/scala/com/janboerman/aoc2019/day16/Day16.scala
@@ -1,8 +1,35 @@
 package com.janboerman.aoc2019.day16
 
+import scala.io.Source
+
 object Day16 extends App {
 
     val fileName = "src/main/resources/day16input.txt"
+    val numbers = Source.fromFile(fileName).getLines().next().toSeq.map(_ - '0')
 
+    val sequence: LazyList[Int] = LazyList.continually(LazyList(0, 1, 0, -1)).flatten
+
+    //val numbers = Seq(1, 2, 3, 4, 5, 6, 7, 8)
+
+    var iteration: Seq[Int] = numbers
+    for (_ <- 0 until 100) {
+        /*I dont understand why toList is required in order to avoid a StackOverflowError...*/
+        iteration = LazyList.fill(numbers.size)(iteration).zipWithIndex.map({case (series, index) => process(index, series)}).toList
+    }
+    val result1 = iteration.take(8).mkString
+    println(result1)
+
+
+    def process(index: Int, sequence: Seq[Int]): Int = {
+        val combineWith = getSequence(index)
+        val sum = sequence.zip(combineWith).map({case (one, two) => one * two}).sum
+        val digit = Math.abs(sum) % 10
+        digit
+    }
+
+    def getSequence(index: Int): LazyList[Int] = {
+        val repeat = index + 1
+        sequence.flatMap(value => LazyList.fill(repeat)(value)).tail
+    }
 
 }
